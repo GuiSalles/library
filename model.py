f82a535e49400cdcc74654d8c07a1439273619ce
GuiSalles <guilhermexsalles@hotmail.com>
2025-04-28 16:39:28 -0300

    Commit 9: Replacing conditional with polymorphism - part 2.

--- model.py
+++ model.py
@@ -33,28 +33,26 @@ class ChildrenPrice(Price):
         return 1
 
 class Book:
+
     REGULAR: int = 0
     NEW_RELEASE: int = 1
     CHILDREN: int = 2
 
     def __init__(self, title: str, price_code: int):
         self.title = title
-        self.set_price_code(price_code)
-
-    def set_price_code(self, price_code: int):
-        if price_code == Book.REGULAR:
-            self.price = RegulaPrice()
-        elif price_code == Book.NEW_RELEASE:
-            self.price = NewReleasePrice()
+        self.price = self.create_price(price_code)
+    
+    def create_price(self, price_code: int):  
+        if price_code == Book.NEW_RELEASE:
+            return NewReleasePrice()
         elif price_code == Book.CHILDREN:
-            self.price = ChildrenPrice()
-        else:
-            raise ValueError("Invalid price code")
-
-    def get_charge(self, days_rented: int) -> float:
+            return ChildrenPrice()
+        return RegulaPrice()
+    
+    def get_charge(self, days_rented: int):
         return self.price.get_charge(days_rented)
 
-    def get_frequent_renter_points(self, days_rented: int) -> int:
+    def get_frequent_renter_points(self, days_rented: int):
         return self.price.get_frequent_renter_points(days_rented)
 
 class Rental:
